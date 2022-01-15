10c7c4cd55bcb33c44a0149ca263e9765d4b4647
Aayan2006-glitch <72450718+Aayan2006-glitch@users.noreply.github.com>
2022-01-15 13:11:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD1XW6zzF6sMPnJPUQDZz2zMEWDQ8iyJtQ",
+    authDomain: "story-app-72416.firebaseapp.com",
+    databaseURL: "https://story-app-72416-default-rtdb.firebaseio.com",
+    projectId: "story-app-72416",
+    storageBucket: "story-app-72416.appspot.com",
+    messagingSenderId: "253415369593",
+    appId: "1:253415369593:web:d002c41694a01dd2465da8"
+  };
+  
